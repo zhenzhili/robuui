15c615b53b4c9a35157bc18ad58e558bac22ce7b
lizhenzhi <lzz19870915xss>
2016-06-28 18:41:30 +0800

    init

--- scripts/app.js
+++ scripts/app.js
@@ -395,23 +395,30 @@ angular
             }
         ];
     })
-    .controller('chartJsLineCtrl',function ($scope) {
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.series = ['Series A', 'Series B'];
-        $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
+    .controller('chartJsLineCtrl',function ($scope,$timeout) {
+        $timeout(function () {
+            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];
+        },100);
     })
-    .controller('chartJsBarCtrl',function ($scope) {
-        $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-        $scope.series = ['Series A', 'Series B'];
+    .controller('chartJsBarCtrl',function ($scope,$timeout) {
+        $timeout(function () {
+            $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+            $scope.series = ['Series A', 'Series B'];
 
-        $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
+            $scope.data = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];
+        },100);
     })
     .controller('calCtrl',function ($scope) {
-
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
     });
